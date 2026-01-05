fbb87cce583a7d6bd258ba964154c81600444e5b
Sumit <sumitbudhathoki80@gmail.com>
2026-01-05 12:35:40 +0545

    login.jsx file created. API calling form frontend

---
+++ frontend/login.jsx
@@ -0,0 +1,16 @@
+const login = async () => {
+  const res = await fetch("http://localhost/api/login.php", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username,
+      password
+    })
+  });
+
+  const data = await res.json();
+
+  if (data.token) {
+    localStorage.setItem("token", data.token);
+  }
+};
